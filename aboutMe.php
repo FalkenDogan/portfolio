0a76f7228fae676d66bc5136f614bf5af15ba73b
FalkenDogan <s.mustafa.dogan@gmail.com>
2024-08-11 21:58:15 +0200

    first commit

---
+++ aboutMe.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto"> 
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="aboutMe.php">About Me</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="law.php">Law</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="it.php">IT</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="writter.php">Writer</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="travel.php">Travel</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <h1>ABOUT ME PAGE</h1>
+    <div>
+      <p class="aboutMeText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nesciunt, earum consequuntur quam, tempore, deserunt eligendi nostrum ut debitis reprehenderit provident. Deleniti architecto molestiae quidem fuga porro, vero provident laborum impedit perferendis ullam reprehenderit ad voluptatem obcaecati suscipit accusantium eius necessitatibus aut a quaerat dolor natus consequuntur delectus harum magni.</p> 
+    </div>
+  
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
